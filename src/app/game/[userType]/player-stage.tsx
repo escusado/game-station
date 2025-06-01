172e3d7e47f0057801091df48779f8bbbb006d33
escusado <escusado@gmail.com>
2025-05-31 22:15:03 -0700

    adds correct data transmission from client to station

--- src/app/game/[userType]/player-stage.tsx
+++ src/app/game/[userType]/player-stage.tsx
@@ -1,27 +1,116 @@
-import { RoomContext } from "@livekit/components-react";
-import { FC, useContext, useEffect } from "react";
+import { ConnectionState, RoomContext } from "@livekit/components-react";
+import { FC, useContext, useEffect, useState, useRef } from "react";
+
+const styles = /* css */ `
+.app-client {
+  width: 100%;
+  height: 100%;
+  background-color: #FF3399;
+  display: flex;
+  flex-direction: column;
+}
+
+.app-client p {
+  margin: 0;
+}
+
+.button {
+  width: 100px;
+  height: 50px;
+  background-color: #4CAF50;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  color: #333;
+  justify-content: center;
+}
+
+.status {
+  fzont-size: 8px;
+}
+`;
 
 type PlayerStageProps = {
   className?: string;
 };
 
 const PlayerStage: FC<PlayerStageProps> = ({ className }) => {
-  const context = useContext(RoomContext);
+  const room = useContext(RoomContext);
+
+  const [accelerometerStatus, setAccelerometerStatus] = useState({});
+  const [gyroStatus, setGyroStatus] = useState({});
+
+  const accelerometerStatusRef = useRef({ x: 0, y: 0, z: 0 });
+  const gyroStatusRef = useRef({ alpha: 0, beta: 0, gamma: 0 });
 
   useEffect(() => {
-    if (context) {
-      setInterval(async () => {
-        console.log("PlayerStage is running");
-        await context.localParticipant.sendText("wazzup", {
+    if (!room) return;
+
+    const intervalId = setInterval(async () => {
+      await room.localParticipant.sendText(
+        JSON.stringify({
+          accelerometerStatus: accelerometerStatusRef.current,
+          gyroStatus: gyroStatusRef.current,
+        }),
+        {
           topic: "game",
-        });
-      }, 1000);
+        },
+      );
+    }, 1000);
 
-      return () => {};
+    return () => clearInterval(intervalId);
+  }, [room]); // Only depends on room now
+
+  const requestDeviceSensorAccess = async () => {
+    try {
+      // @ts-expect-error DeviceMotionEvent and DeviceOrientationEvent are not defined in all environments
+      DeviceMotionEvent.requestPermission();
+      // @ts-expect-error DeviceOrientationEvent is not defined in all environments
+      DeviceOrientationEvent.requestPermission();
+
+      window.addEventListener("devicemotion", (event) => {
+        const { x, y, z } = event.accelerationIncludingGravity || {
+          x: 0,
+          y: 0,
+          z: 0,
+        };
+        setAccelerometerStatus({ x, y, z });
+        // @ts-expect-error DeviceMotionEvent is not defined in all environments
+        accelerometerStatusRef.current = { x, y, z };
+      });
+
+      window.addEventListener("deviceorientation", ({ alpha, beta, gamma }) => {
+        // @ts-expect-error DeviceOrientationEvent is not defined in all environments
+        gyroStatusRef.current = { alpha, beta, gamma };
+        setGyroStatus({ alpha, beta, gamma });
+      });
+    } catch (e) {
+      console.log("Error requesting access: " + JSON.stringify(e));
+      return;
     }
-  }, [context]);
+  };
 
-  return <div className={`${className} bg-red-200`}>PlayerStage</div>;
+  return (
+    <div className={`${className} bg-red-200`}>
+      PlayerStage
+      <>
+        <style>{styles}</style>
+        <div className="app-client">
+          <div className="button" onClick={requestDeviceSensorAccess}>
+            Request access
+          </div>
+          <div className="status">
+            <pre>{JSON.stringify(accelerometerStatus, null, 2)}</pre>
+          </div>
+          <div className="status">
+            <pre>{JSON.stringify(gyroStatus, null, 2)}</pre>
+          </div>
+        </div>
+      </>
+    </div>
+  );
 };
 
 export default PlayerStage;
